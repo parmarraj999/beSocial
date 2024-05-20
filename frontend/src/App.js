33aca71483741492a7f544b47d778967408a7dbb
parmarraj999 <rajraja5122@gmail.com>
2024-05-20 17:42:12 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Nav from "./layouts/nav/nav.js";
+import Main from "./layouts/main/main.js";
+
+function App() {
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
