a28048fb599baae8c76f48e401a87d15341a5d8e
parmarraj999 <rajraja5122@gmail.com>
2024-05-20 23:49:39 +0530

    getCommit

---
+++ frontend/src/layouts/auth/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./auth.css"
+
+function Login() {
+    return (
+        <div className='form-container'>
+            <div className='form-box'>
+
+            </div>
+            <div className='form-btn' >
+                
+            </div>
+        </div>
+    )
+}
+
+export default Login
--- frontend/src/layouts/auth/auth.js
+++ frontend/src/layouts/auth/auth.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./auth.css"
 import Welcome from '../welcome/welcome'
+import Login from './Login'
 
 function Auth() {
+  
+  const [form,setForm] = useState("login")
+
   return (
     <div className='auth-container'>
       <Welcome/>
+     {
+      form === "login" ?
+      <Login/> : ""
+     }
     </div>
   )
 }
