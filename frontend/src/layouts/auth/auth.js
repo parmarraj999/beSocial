bdd9580cc2ff9c4248353279c20e2699ecf2682c
MOHIT-D360 <mohitnsp786@gmail.com>
2024-05-20 21:24:17 +0530

    working auth

--- frontend/src/layouts/auth/auth.js
+++ frontend/src/layouts/auth/auth.js
@@ -5,10 +5,20 @@ import Welcome from '../welcome/welcome'
 function Auth() {
   return (
     <div className='auth-container'>
-      <Welcome/>
-      
+      {/* <Welcome/> */}
+      <div className='auth-wrapper' >
+        <div className='auth-box'>
+          <h1></h1>
+        </div>
+        <div className='auth-btn-container'>
+          <button className='first-btn'>Log In</button>
+          <button>Create Account</button>
+        </div>
+      </div>
+
     </div>
   )
 }
 
 export default Auth
+//#eaecd1
