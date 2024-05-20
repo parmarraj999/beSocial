65bca88a4ed5973ead42e90f069ad4acb005819f
MOHIT-D360 <mohitnsp786@gmail.com>
2024-05-20 17:54:57 +0530

    commit by mohit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,4 @@
 import "./App.css"
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Nav from "./layouts/nav/nav.js";
-import Main from "./layouts/main/main.js";
 
 function App() {
 
