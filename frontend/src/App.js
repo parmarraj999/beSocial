c65db215af8b639468718d86b19796f7e13ca6e8
parmarraj999 <rajraja5122@gmail.com>
2024-05-20 17:53:41 +0530

    check

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,10 @@
 import "./App.css"
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Nav from "./layouts/nav/nav.js";
-import Main from "./layouts/main/main.js";
 
 function App() {
 
   return (
     <div className="App">
-      
+      <h1>BeSocial</h1>
     </div>
   );
 }
