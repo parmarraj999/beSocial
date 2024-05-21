46c1a832465bf961138354cb7517c572687b0ed4
MOHIT-D360 <mohitnsp786@gmail.com>
2024-05-21 12:43:06 +0530

    btn-animation

--- frontend/src/layouts/auth/auth.js
+++ frontend/src/layouts/auth/auth.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import "./auth.css"
 import Welcome from '../welcome/welcome'
 import Login from './Login'
+// import doodle-one from 'public/image/doodle-one.png'
+
 
 function Auth() {
   
@@ -12,15 +14,16 @@ function Auth() {
       {/* <Welcome/> */}
       <div className='auth-wrapper' >
         <div className='auth-box'>
-          <h1></h1>
+          
+          
         </div>
         <div className='auth-btn-container'>
           <button className='first-btn'>Log In</button>
-          <button>Create Account</button>
+          <button className='second-btn'><h3>Create Account</h3></button>
         </div>
       </div>
 
-      <Welcome/>
+      {/* <Welcome/> */}
      {
       form === "login" ?
       <Login/> : ""
