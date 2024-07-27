6c33445a02aa5255e04fe442de79b695434c04ad
parmarraj999 <rajraja5122@gmail.com>
2024-07-28 03:00:22 +0530

    working on follow list

--- frontend/src/layout/home/post/postCard.js
+++ frontend/src/layout/home/post/postCard.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+  import React, { useContext, useEffect, useState } from 'react'
 import './post.css'
 import { UserDataContext } from '../../../context/context'
 import axios from 'axios';
---
+++ frontend/src/layout/home/post/postProfile/postProfile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './postProfile.css';
+
+function PostProfile() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default PostProfile
