57c0d56a97c13a7744acf6eb8c967bfcb72e6d88
parmarraj999 <rajraja5122@gmail.com>
2024-08-14 21:51:11 +0530

    access control error

--- backend/index.js
+++ backend/index.js
@@ -9,8 +9,8 @@ require("dotenv").config();
 
 const app = express();
 const corsOptions ={
-  origin:'http://localhost:3000', 
-  credentials:true,            //access-control-allow-credentials:true
+  origin:'http://localhost:3000/', 
+  credentials:true,           
   optionSuccessStatus:200
 }
 app.use(cors(corsOptions))
